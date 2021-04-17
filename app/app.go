4432b36c91a6edb58e07e4b30c70bb6115e7d97d
Minh Nguyen Hong <minh.nguyen@encapital.io>
2021-04-17 10:00:00 +0700

    add cors

--- app/app.go
+++ app/app.go
@@ -15,9 +15,24 @@ func NewApp() *App {
 		port: ":3000",
 	}
 }
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 func (app *App) Init() {
 	engine := gin.Default()
+	engine.Use(CORSMiddleware())
 	engine.LoadHTMLGlob("templates/*")
 	engine.GET("/health", func(ctx *gin.Context){
 		ctx.JSON(200, gin.H{
